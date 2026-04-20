627e9d104967ea8f7c0a7f181b570e5f5827def2
Chronus432 <jmkenn1111@protonmail.com>
2026-04-20 21:45:07 +1000

    Auto-commit: emergent-agent-e1 for d8ff80bd-9189-4ed9-b5d3-e5e8f780f2b6

---
+++ backend/server.py
@@ -0,0 +1,26 @@
+# Code changes to update GoGet.Fit references to Bloom& Breathe in backend/server.py
+
+
+# Previous content removed and the below changes applied
+
+## backend/server.py
+
+---
+Line 16:  
+- db_name = 'gogetfit_db'
++ db_name = 'bloom_breathe_db'
+
+---
+Line 37:  
+- logger.info("Starting up GoGet.Fit API...")
++ logger.info("Starting up Bloom& Breathe API...")
+
+---
+Line 55:  
+- title = "GoGet.Fit API"
++ title = "Bloom& Breathe API"
+
+---
+Line 67:  
+- logger.info("GoGet.Fit API is running")
++ logger.info("Bloom& Breathe API is running")
